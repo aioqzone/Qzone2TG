1d038ef4b6491be699f57f45322b2ae631c97140
JamzumSum <zzzzss990315@gmail.com>
2021-08-05 15:22:42 +0800

    add a real json parser for irregular json

--- test/demjsonTest.py
+++ test/demjsonTest.py
@@ -4,12 +4,22 @@ from unittest import TestCase
 
 import demjson
 from qzonebackend.qzone import json_loads
-"""
-testDemjson (demjsonTest.TestSpeed) ... 2.585096836090088 s
-ok
-testRegulate (demjsonTest.TestSpeed) ... 0.49105286598205566 s
-ok
-"""
+
+# testDemjson (demjsonTest.TestSpeed) ... 1.7905035018920898 s
+# ok
+# testRegulate (demjsonTest.TestSpeed) ... 0.011957883834838867 s
+# ok
+
+
+class AccTest(TestCase):
+    def setUp(self):
+        with open('tmp/demjson.json', encoding='utf8') as f:
+            self.j = f.read()
+
+    def testAcc(self):
+        m = json_loads(self.j)
+        d = demjson.decode(self.j)
+        self.assertDictEqual(d, m)
 
 
 class TestSpeed(TestCase):
