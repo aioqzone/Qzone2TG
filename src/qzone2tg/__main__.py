c7f8c3ce327508fe8340151c0456cefcb5d0737d
JamzumSum <zzzzss990315@gmail.com>
2021-11-16 15:31:12 +0800

    update doc main: add more check, constrains, exc handler   change some translate   add check new version   fix some bugs   a little refactor doc: update example config bump version to rc1

--- src/qzone2tg/__main__.py
+++ src/qzone2tg/__main__.py
@@ -3,15 +3,16 @@ import logging
 import logging.config
 import sys
 from getpass import getpass
+from pathlib import Path
 
 from omegaconf import OmegaConf
 from omegaconf.dictconfig import DictConfig
-from omegaconf.listconfig import ListConfig
 
 from qzone2tg.frontend.tg import PollingBot, RefreshBot, WebhookBot
 from qzone2tg.middleware.storage import TokenTable
 from qzone2tg.qzone.feed import FeedDB, QzCachedScraper
 from qzone2tg.qzone.scraper import QzoneScraper
+from qzone2tg import __version__ as QZ_VER
 
 NO_INTERACT: bool = True
 NAME_LOWER = 'qzone2tg'
@@ -23,37 +24,44 @@ def getPassword(qzone: DictConfig):
     if strategy == 'force': return qzone
 
     if PWD_KEY in qzone:
-        assert NO_INTERACT, "password can be passed by CLI only when no-interact"
-        return
+        if not NO_INTERACT:
+            logger.fatal("只有无交互模式下才可以从 CLI 传递密码")
+            exit(1)
 
     try:
         import keyring
         pwd = keyring.get_password(NAME_LOWER, str(qzone.qq))
     except ImportError:
+        logger.error("keyring 未导入", exc_info=True)
         pwd = None
+        keyring = None
 
     if not pwd and not NO_INTERACT:
         pwd = getpass(f'输入密码{"" if strategy == "forbid" else " (按回车跳过)"}:')
-        if (pwd := pwd.strip()): keyring.set_password(NAME_LOWER, str(qzone.qq), pwd)
+        if keyring and (pwd := pwd.strip()):
+            try:
+                keyring.set_password(NAME_LOWER, str(qzone.qq), pwd)
+            except:
+                logger.error('自动设置密码失败. 请您参考 wiki 手动保存密码.', exc_info=True)
+
     if not (pwd and pwd.strip()):
         d = {
             'forbid': lambda: ValueError('config: No password specified.'),
-            'prefer': lambda: logging.info('密码未指定. qr_strategy 从 `prefer` 改为 `force`.'
+            'prefer': lambda: logging.info('密码未指定. qr_strategy 从 prefer 改为 force.'
                                            ) or qzone.__setitem__('qr_strategy', 'force'),
-            'allow': lambda: logging.warning('密码未指定. qr_strategy 从 `allow` 改为 `force`.') or qzone.
+            'allow': lambda: logging.warning('密码未指定. qr_strategy 从 allow 改为 force.') or qzone.
             __setitem__('qr_strategy', 'force')
         }
         if (e := d[strategy]()): raise e
+
     qzone[PWD_KEY] = pwd
     return qzone
 
 
 def configLogger(log_conf: DictConfig):
-    if 'conf' in log_conf:
-        try:
-            logging.config.fileConfig(log_conf.conf, disable_existing_loggers=False)
-        except FileNotFoundError as e:
-            print(str(e))
+    global logger
+    if 'conf' in log_conf and log_conf.conf and Path(log_conf.conf).exists():
+        logging.config.fileConfig(log_conf.conf, disable_existing_loggers=False)
     else:
         default = {
             'format': '[%(levelname)s] %(asctime)s %(name)s: %(message)s',
@@ -62,33 +70,32 @@ def configLogger(log_conf: DictConfig):
         }
         default.update(log_conf)
         logging.basicConfig(**default)
-    global logger
-    logger = logging.getLogger("Main")
+    logger = logging.getLogger(NAME_LOWER + '.main')
 
 
 def configVersionControl(conf: DictConfig):
     fw = lambda msg: logger.warning("FutureWarning: " + msg)
-    if isinstance(conf.bot.accept_id, ListConfig):
-        fw("在未来的版本中, `bot.accept_id`被视为 `int`. 只有列表中的第一个值生效.")
-        conf.bot.accept_id = conf.bot.accept_id[0]
+
     if 'proxy' in conf.bot:
-        fw("从`2.2.1b4`起, `bot.proxy`重命名为`bot.network`. `network`配置段支持包括代理在内的更多设置. 查看wiki获取更多信息. ")
+        fw("从 2.2.1b4 起, bot.proxy 重命名为 bot.network. network 配置段支持包括代理在内的更多设置. 查看 wiki 获取更多信息. ")
         conf.bot['network'] = conf.bot.pop('proxy')
     if 'auto_start' in conf.bot:
-        fw("`auto_start`已在多个版本中弃用. 此条目在`2.2.1b4`中移除 .")
+        fw("auto_start 已在多个版本中弃用. 此条目在 2.2.1b4 中移除 .")
         conf.bot.pop('auto_start')
 
     return conf
 
 
 def dueWithConfig(conf: DictConfig, NO_INTERACT=False):
-    conf.setdefault('qzone', {})
-    conf.setdefault('log', {})
-    conf.setdefault('feed', {})
+    edf = lambda: DictConfig({}, parent=conf)
+    conf.setdefault('qzone', edf())
+    conf.setdefault('log', edf())
+    conf.setdefault('feed', edf())
 
+    configLogger(conf.log or {})
     conf = configVersionControl(conf)
 
-    if 'qq' in conf.qzone:
+    if conf.qzone.qq:
         print('QQ:', conf.qzone.qq)
     elif NO_INTERACT:
         logger.fatal('无交互模式: QQ未指定')
@@ -99,29 +106,38 @@ def dueWithConfig(conf: DictConfig, NO_INTERACT=False):
     return conf
 
 
-def checkUpdate():
+def checkUpdate(proxy: str = None):
     from qzemoji import DBMgr
+    if proxy: DBMgr.proxy = proxy
     DBMgr.autoUpdate('data/emoji.db')
     logger.debug('emoji db upgraded')
+
+    from updater.github import Repo, GhUpdater, register_proxy
+    from updater.utils import version_filter
+    if proxy: register_proxy({'http': proxy, 'https': proxy})
+    up = GhUpdater(Repo('JamzumSum', 'Qzone2TG'))
+    vf = version_filter(up, f'>{QZ_VER}', 1, pre=True)
+    vf = list(vf)
+    if vf and not 'a' in vf[0].tag and not 'dev' in vf[0].tag:
+        logger.info(f"Qzone2TG {vf[0].tag} availible. Current version is {QZ_VER}.")
 
 
 def main(args):
     ca = OmegaConf.from_cli(args)
     cd = OmegaConf.load(CONF_PATH)
     d = OmegaConf.merge(cd, ca)
-    configLogger(d.get('log', {}))
 
     if 'password' in cd.get('qzone', {}):
-        logger.fatal(
+        logging.fatal(
             'Qzone2TG 使用系统keyring存储密码. 使用以下命令设置密码: \n\n'
-            f"`keyring set qzone2tg {d.qzone.qq} 密码`\n\n"
+            f"`keyring set qzone2tg {d.qzone.qq or '账号'} 密码`\n\n"
             "然后从配置文件中移除`qzone.password` 和 `qzone.savepwd` 并重启程序.\n"
             "PS: passing password from cli is allowed."
         )
         exit(1)
 
     d = dueWithConfig(d, NO_INTERACT)
-    logger.debug("config loaded")
+    logger.debug(f"config loaded {d}")
 
     tg_plugin_def = {'tg': {'is_sent': 'BOOLEAN default 0'}}
     db = FeedDB(f"data/{d.qzone.qq}.db", **d.feed, plugins=tg_plugin_def)
@@ -131,7 +147,7 @@ def main(args):
     feedmgr = QzCachedScraper(spider, db)
     logger.debug('crawler OK')
 
-    checkUpdate()
+    checkUpdate(d.bot.get('network', {}).get('proxy_url', None))
 
     method = d.bot.pop('method', None)
     if 'webhook' in d.bot:
@@ -141,6 +157,7 @@ def main(args):
                 "Webhook is used in this case."
             )
         method = 'webhook'
+    method = method or 'polling'
     BotCls = {'polling': PollingBot, 'webhook': WebhookBot, "refresh": RefreshBot}[method]
     bot: RefreshBot = BotCls(feedmgr=feedmgr, **d.bot)
     bot.addBlockUin(d.qzone.qq)
